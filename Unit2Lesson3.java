0dc9ccba27457df3d2b201c86e78b0f734c6788d
HelenHetrick <94848906+HelenHetrick@users.noreply.github.com>
2022-06-13 13:05:55 +0000

    Initial commit

---
+++ Unit2Lesson3.java
@@ -0,0 +1,19 @@
+public class Unit2Lesson3
+{
+public static void main(String [] args)
+{
+String word="Computer Science";
+System.out.println("The length of the word is "+word.length());
+
+String s1="at";
+String s2="At";
+String s3="at";
+
+boolean ans1=s1.equals(s2);
+boolean ans2=s1.equals(s3);
+System.out.println("Does s1 equal s2? "+ans1);
+System.out.println("Does s1 equal s3? "+ans2);
+
+}
+}
+
